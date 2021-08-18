15e606360876b4d78958d66f9db4b3806032f66d
ddarkCode <royal.ugoh@gmail.com>
2021-08-18 13:11:59 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div class='profile-img'>
+          <img src='/images/coder.jpg' alt='Ugo' />
+        </div>
+        <div class='profile-details'>
+          <div class='details'>
+            <div class='info'>
+              <h2>Nnamani Eze Chidozie Ugochukwu</h2>
+              <p>Front-end developer</p>
+            </div>
+            <div class='contacts'>
+              <span class='material-icons'> mail </span>
+              <span>royal.ugoh@gmail.com</span>
+            </div>
+          </div>
+          <p>
+            I am a detail-oriented self-taught developer, who is willing to
+            learn and create outstanding UI applications. Over the course of
+            learning, I have developed proven problem-solving and communication
+            skills. I am adept in bringing forth expertise in design
+            implementation, installation, and maintenance of software systems.
+            Equipped with a diverse and promising skill-set. Proficient in
+            various platforms and the JavaScript ecosystem. Able to effectively
+            self-manage during independent projects, as well as collaborate as
+            part of a productive team.
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
